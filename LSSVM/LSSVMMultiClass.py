33525abd4fe983d0398ebbbfe06c78295ace4b38
wangxin <simonlight90@gmail.com>
2015-11-21 03:27:33 +0100

    beautiful code ready for test

--- LSSVM/LSSVMMultiClass.py
+++ LSSVM/LSSVMMultiClass.py
@@ -211,13 +211,13 @@ def main():
     #parameters
     lambdaCV = [1e-4]
     epsilonCV = [1e-3]
-    categories = ["horse"]
-#     categories = [sys.argv[1]]
-    scaleCV = [90]    
-#     scaleCV = [int(sys.argv[2])]    
-    tradeoffCV = [0.1]
+#     categories = ["horse"]
+    categories = [sys.argv[1]]
+#     scaleCV = [90]    
+    scaleCV = [int(sys.argv[2])]    
+#     tradeoffCV = [0.1]
 #     tradeoffCV = [float(sys.argv[3])]
-#     tradeoffCV = [0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0]
+    tradeoffCV = [0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0]
     initializedType = "noInit"
     hnorm = False;
     
