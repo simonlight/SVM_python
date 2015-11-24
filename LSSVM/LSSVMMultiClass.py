8e2e04123ea3911120a456850130547235030ae6
xin_urbandancesquad <xin.wang@lip6.fr>
2015-11-24 10:57:56 +0100

    cat ????

--- LSSVM/LSSVMMultiClass.py
+++ LSSVM/LSSVMMultiClass.py
@@ -187,9 +187,9 @@ def train_phase(resDir, classifier_folder,\
 def main():
     
     # big    stefan
-    sourceDir = "/home/wangxin/Data/full_stefan_gaze/"
-    resDir = "/home/wangxin/results/full_stefan_gaze/std_et/"
-    gazeType = "stefan"
+#     sourceDir = "/home/wangxin/Data/full_stefan_gaze/"
+#     resDir = "/home/wangxin/results/full_stefan_gaze/std_et/"
+#     gazeType = "stefan"
     
     # local stefan
 #     sourceDir = "/local/wangxin/Data/gaze_voc_actions_stefan/"
@@ -197,9 +197,9 @@ def main():
 #     gazeType = "stefan"
 
     # big ferrari
-#     sourceDir = "/home/wangxin/Data/ferrari_gaze/"
-#     resDir = "/home/wangxin/results/ferrari_gaze/std_et/"
-#     gazeType = "ferrari"
+    sourceDir = "/home/wangxin/Data/ferrari_gaze/"
+    resDir = "/home/wangxin/results/ferrari_gaze/std_et/"
+    gazeType = "ferrari"
          
 #     local ferrari
 #     sourceDir = "/local/wangxin/Data/ferrari_gaze/";
